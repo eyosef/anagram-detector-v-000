294a473ee2903d322bd025fff3e088cfe40bd30b
Eden Mesfun <emesfun@gmail.com>
2018-02-07 16:12:36 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,10 +8,7 @@ class Anagram
 
   def match(list_of_words)
     nested = []
-    #split_word = []
     result = []
-
-    #split_word << @word.split(//).sort.join
 
     nested << list_of_words.map { |array| array.split(//).sort.uniq.join}
     nested.flatten.each_with_index {|letters, index| result << letters[index] if letters[index] == @word}
