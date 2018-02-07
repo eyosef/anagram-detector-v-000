406ebafea09164772f4512e95cd77be3009714ca
Eden Mesfun <emesfun@gmail.com>
2018-02-07 15:57:42 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,7 +12,7 @@ class Anagram
     result = []
 
     split_word << @word.split(//).sort.join
-    nested << list_of_words.each { |array| array.split(//).uniq.sort.join}
+    nested << list_of_words.each { |array| array.split(//).sort.join}
     nested.each_with_index {|letters, index| result << nested[index] if letters == split_word}
     result
     binding.pry
