35cfb1d4d27b5d4559a841f11b9f64137fc70ee3
Eden Mesfun <emesfun@gmail.com>
2018-02-07 16:06:03 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -14,7 +14,7 @@ class Anagram
     split_word << @word.split(//).sort.join
     nested << list_of_words.map { |array| array.split(//).sort.uniq.join}
     nested.flatten
-    nested.each_with_index {|letters, index| result << nested[index] if letters == split_word}
+    nested.each_with_index {|letters, index| result << nested[index] if letters == @word}
     result
 
 
