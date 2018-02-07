1247dec648076ec614384cd0f7050fe4f06ee7fc
Eden Mesfun <emesfun@gmail.com>
2018-02-07 15:26:27 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,6 +11,7 @@ class Anagram
     split_word = []
     split_word << word.split(//).sort
     nested << list_of_words.map { |array| array.split(//).uniq.sort}
+    nested.each_with_index{|letters, index| print letters[index] if letters == split_word}
         binding.pry
   end #method
 
