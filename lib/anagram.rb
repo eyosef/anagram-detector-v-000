2d86e975beaf8c8b1d6d164631901ff3919126c8
Eden Mesfun <emesfun@gmail.com>
2018-02-07 16:12:56 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,7 +11,7 @@ class Anagram
     result = []
 
     nested << list_of_words.map { |array| array.split(//).sort.uniq.join}
-    nested.flatten.each_with_index {|letters, index| result << letters[index] if letters[index] == @word}
+    nested.flatten.each_with_index {|word, index| result << letters[index] if letters[index] == @word}
     result
   end #method
 end #class
