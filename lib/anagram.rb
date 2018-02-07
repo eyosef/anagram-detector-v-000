0e39cdee7b01d7c420e575f59648da7accae93cb
Eden Mesfun <emesfun@gmail.com>
2018-02-07 13:52:14 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,7 @@ class Anagram
   def match(list_of_words)
     #nested_array = []
     list_of_words.map {|string| string.split.uniq}
-    list_of_words.each {|word| word.split(/\w/)
+    list_of_words.each {|word| word.split(/\w/)}
     binding.pry
   end
 
