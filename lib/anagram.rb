ed69a5f7caca2a73b32d8b6b972240c0c7325b8d
Eden Mesfun <emesfun@gmail.com>
2018-02-07 15:14:31 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,12 +10,9 @@ class Anagram
     split_word = []
     split_word << word.split(//)
     list_of_words.map do |array| array.split(//).uniq.sort
-      array.each_line do |string|
-
         if array == split_word
           return string
         end #if statement
-      end #each_line
     end #map iteration
   end #method
 
